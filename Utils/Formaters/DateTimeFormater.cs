89966040fe183115ec22b759daad1b7c3cb90641
jackie.liu <jackie.liu@zuru.com>
2025-05-23 14:09:34 +0800

    feat: 添加工具类

---
+++ Utils/Formaters/DateTimeFormater.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Utils.StringExtensions;
+
+namespace Utils.Formaters
+{
+    /// <summary>
+    ///  返回前端的时候，格式化时间
+    /// </summary>
+    public class DateTimeFormater : JsonConverter
+    {
+        private readonly string _dateFormat;
+
+        public DateTimeFormater(string dateFormat)
+        {
+            _dateFormat = dateFormat ?? throw new ArgumentNullException(nameof(dateFormat));
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime?) || objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+
+            var dateString = reader.Value.ToString();
+            if (dateString.IsNullOrEmpty())
+            {
+                return null;
+            }
+            if (DateTime.TryParseExact(dateString, _dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+            {
+                return date;
+            }
+            return reader.Value; // 如果解析失败，返回 null
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is DateTime dateTime)
+            {
+                writer.WriteValue(dateTime.ToString(_dateFormat, CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                writer.WriteNull();
+            }
+        }
+    }
+}
---
+++ Utils/StringExtensions/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.StringExtensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+        public static bool IsNullOrEmpty(this object thisValue) => thisValue == null || thisValue == DBNull.Value || string.IsNullOrWhiteSpace(thisValue.ToString());
+
+    }
+}
